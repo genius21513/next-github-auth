5d63e79d2b808431c4acc58d11ee35eeb7c59b48
genius21513 <96323805+genius21513@users.noreply.github.com>
2023-01-12 02:35:01 +0900

    Middle stop

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
+/* eslint-disable import/no-anonymous-default-export */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { unstable_getServerSession } from "next-auth/next"
+import { authOptions } from "./auth/[...nextauth]"
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async (req, res) => {
+  const session = await unstable_getServerSession(req, res, authOptions)
+
+  if (session) {
+    res.send({
+      content:
+        "This is protected content. You can access this content because you are signed in.",
+    })
+  } else {
+    res.send({
+      error: "You must be signed in to view the protected content on this page.",
+    })
+  }
 }
